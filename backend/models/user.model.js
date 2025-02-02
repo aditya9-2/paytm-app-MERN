7bb3a33d0ae02a72d8051f5dcb0961541ddef54a
Aditya <adityabasak7031@gmail.com>
2025-02-02 21:50:32 +0530

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+export default mongoose.model("User", userSchema);
