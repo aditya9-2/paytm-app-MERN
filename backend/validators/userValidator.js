2b6a01a69d03d2024fac93b35783a89ee40b1118
Aditya <adityabasak7031@gmail.com>
2025-02-02 21:59:52 +0530

    validate user by zod

---
+++ backend/validators/userValidator.js
@@ -0,0 +1,29 @@
+import { z } from "zod"
+
+const userValidateSchema = z.object({
+
+    username: z.string().min(3, "username should contain atleast 3 characters").max("username can have at most 15 characters"),
+    firstName: z.string().min(3, "first name should be minimum 3 characters ").max(30, "first name maximum can contain 30 characters"),
+    lastName: z.string().min(3, "last name should be minimum 3 characters ").max(30, "last name maximum can contain 30 characters"),
+    password: z.string().min(6, "minimum 6 characters required").max(18, "ppassword maximum can contain 18 characters")
+});
+
+const validateUserSignup = (data) => {
+
+    try {
+        userValidateSchema.parse(data);
+        return {
+            sucess: true
+        }
+
+    } catch (err) {
+
+        return {
+            success: false,
+            error: err.errors.map(e => e.message).join(", ")
+        }
+
+    }
+}
+
+export default validateUserSignup
