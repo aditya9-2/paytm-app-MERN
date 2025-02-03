59c61cf58b7b3e9518761dbacb97de7ac5301fdd
Aditya <adityabasak7031@gmail.com>
2025-02-03 19:13:03 +0530

    add transfer money route

--- backend/routers/accountRouter.js
+++ backend/routers/accountRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import getUserBalance from "../controllers/account/getUserBalance.controller.js";
 import userAuthToken from "../middlewares/userAuthMiddleware.js";
+import transferMoney from "../controllers/account/transferMoney.controller.js";
 
 const router = new Router;
 
-router.get('/balance', userAuthToken, getUserBalance)
+router.get('/balance', userAuthToken, getUserBalance);
+router.post('/transfer', userAuthToken, transferMoney);
 
 
 export default router;
