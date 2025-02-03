3dd2a8d05bb23c7b4f05c84d3549106707ae8e20
Aditya <adityabasak7031@gmail.com>
2025-02-03 19:05:46 +0530

    create fetch balance of authenticated user API

--- backend/controllers/account/getUserBalance.controller.js
+++ backend/controllers/account/getUserBalance.controller.js
@@ -1,4 +1,33 @@
-const getUserBalance = (req, res) => {
+import accountModel from "../../models/account.model.js";
+
+const getUserBalance = async (req, res) => {
+
+    try {
+
+        const account = await accountModel.findOne({ userId: req.userId });
+
+        if (!account) {
+            res.statu(403).json({
+                message: "Incorrect ID",
+
+            });
+            return;
+        }
+
+        res.status(201).json({
+            balance: account.balance
+        })
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: "Internal server error",
+            error: err.message
+        })
+
+    }
+
+
 
 }
 export default getUserBalance;
