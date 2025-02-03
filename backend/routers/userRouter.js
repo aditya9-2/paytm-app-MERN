c875d982a450c2e82995a9c0cabccf11eb3851c6
Aditya <adityabasak7031@gmail.com>
2025-02-03 18:06:52 +0530

    add filter user route

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,12 +3,14 @@ import userSignup from "../controllers/userSignup.controller.js";
 import userSignin from "../controllers/userSignin.controller.js";
 import updateUserInformation from "../controllers/updateUserInformation.controller.js";
 import userAuthToken from "../middlewares/userAuthMiddleware.js";
+import filerUser from "../controllers/filerUser.controller.js";
 
 const router = new Router();
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
 
-router.put("/update", userAuthToken, updateUserInformation)
+router.put("/update", userAuthToken, updateUserInformation);
+router.get("/bulk", filerUser);
 
 export default router
