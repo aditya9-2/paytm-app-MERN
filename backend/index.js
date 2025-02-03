4ed6d979bafc0c2756e20014cbccbaf982fe6233
Aditya <adityabasak7031@gmail.com>
2025-02-03 18:55:07 +0530

    init account router

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js";
+import accountRouter from "./routers/accountRouter.js";
 import connectDB from "./config/db.js";
+
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ app.use(cors({ origin: '*' }))
 app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
+app.use('/api/v1/account', accountRouter);
 
 app.listen(port, async () => {
     await connectDB();
