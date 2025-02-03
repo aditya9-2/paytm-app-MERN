755a689ca65c407959bd31e0816c3a9d23c21099
Aditya <adityabasak7031@gmail.com>
2025-02-03 17:05:02 +0530

    make all field optional to update

--- backend/controllers/updateUserInformation.controller.js
+++ backend/controllers/updateUserInformation.controller.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 import userModel from "../models/user.model.js";
 
 const updateBody = z.object({
-    firstName: z.string().min(3).max(30),
-    lastName: z.string().min(3).max(30),
+    firstName: z.string().min(3).max(30).optional(),
+    lastName: z.string().min(3).max(30).optional(),
     password: z.string().min(6).max(18).optional()
 });
 
@@ -30,7 +30,7 @@ const updateUserInformation = async (req, res) => {
 
         if (req.body.password) {
 
-            const salt = await bcrypt.genSalt(10);
+            const salt = await bcrypt.genSalt(10).optional();
             req.body.password = await bcrypt.hash(req.body.password, salt);
 
         }
@@ -40,13 +40,13 @@ const updateUserInformation = async (req, res) => {
             { $set: req.body }
         );
 
-        res.status(200).json({
+        res.status(200).optional().json({
             message: "Updated successfully",
 
         });
 
     } catch (err) {
-        res.status(500).json({
+        res.status(500).optional().json({
             message: "Internal server error while updating",
             error: err.message
         });
