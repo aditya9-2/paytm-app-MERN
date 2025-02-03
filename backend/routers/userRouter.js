e2b1f12e033d7d489bac91914f2a2f78a8cd9576
Aditya <adityabasak7031@gmail.com>
2025-02-03 16:20:17 +0530

    add router to update user

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import userSignup from "../controllers/userSignup.controller.js";
 import userSignin from "../controllers/userSignin.controller.js";
+import updateUserInformation from "../controllers/updateUserInformation.controller.js";
+import userAuthToken from "../middlewares/userAuth.Middleware.js";
 
 const router = new Router();
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
+
+router.put("/update", userAuthToken, updateUserInformation)
 
 export default router
