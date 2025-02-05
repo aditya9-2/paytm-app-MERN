307243c0c076ffd3e97dbabe4e8ba39de48401bc
Aditya <adityabasak7031@gmail.com>
2025-02-05 18:29:22 +0530

    dynamically rendered minicard

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,18 +1,14 @@
+/* eslint-disable react/prop-types */
 import MiniCard from "./MiniCard";
-import phone from "../assets/phone.png";
-import DTH from "../assets/dth.png";
-import car from "../assets/car.png";
-import viewAll from "../assets/viewAll.png";
 
-const Card = () => {
+const Card = ({ title, items }) => {
   return (
     <div className="bg-white w-[30rem] md:min-w-auto h-64 rounded-2xl mt-20 shadow-2xl border border-gray-100">
-      <h2 className="text-xl font-bold p-4">Recharges</h2>
+      <h2 className="text-xl font-bold p-4">{title}</h2>
       <div className="flex justify-center items-center gap-4 p-4">
-        <MiniCard image={phone} lable={"Mobile Recharge"} />
-        <MiniCard image={DTH} lable={"DTH Reacherge"} />
-        <MiniCard image={car} lable={"FasTag Reacherge"} />
-        <MiniCard image={viewAll} lable={"View All Products"} />
+        {items.map((item, index) => (
+          <MiniCard key={index} image={item.image} lable={item.label} />
+        ))}
       </div>
     </div>
   );
