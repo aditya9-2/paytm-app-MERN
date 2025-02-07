5086126cd0a7412234a55580f778942233177c56
Aditya <adityabasak7031@gmail.com>
2025-02-07 18:32:14 +0530

    add ref in input

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,9 +1,12 @@
+import { forwardRef } from "react";
+
 /* eslint-disable react/prop-types */
-const Input = ({ type, placeholder }) => {
+const Input = forwardRef(({ type, placeholder }, ref) => {
   return (
     <div className="w-80">
       <input
         type={type}
+        ref={ref}
         placeholder={placeholder}
         className="w-full border border-gray-300 rounded-3xl px-4 py-2 text-gray-700 
           focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
@@ -11,6 +14,8 @@ const Input = ({ type, placeholder }) => {
       />
     </div>
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default Input;
