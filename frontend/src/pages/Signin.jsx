5778b2642962a2f9811e8b59273636db2f68291d
Aditya <adityabasak7031@gmail.com>
2025-02-07 18:20:32 +0530

    signin backend integrated

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,80 @@
 /* eslint-disable react/prop-types */
+import { useRef, useState } from "react";
 import Input from "../components/Input";
 import { GrClose } from "react-icons/gr";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signin = ({ toggleSignup, onClose }) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSignin = async () => {
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (!username) {
+      setError("Please enter your username");
+      setTimeout(() => setError(""), 1500);
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter your password");
+      setTimeout(() => setError(""), 1500);
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/v1/user/signin`,
+        {
+          username,
+          password,
+        }
+      );
+
+      const data = response.data;
+      const token = data.token;
+
+      if (data && token) {
+        localStorage.setItem("token", token);
+
+        toast.success("Signin successfull", {
+          position: "bottom-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+
+        setTimeout(() => {
+          onClose();
+          navigate("/dashboard");
+        }, 1600);
+      }
+    } catch (error) {
+      toast.error("Signin failed. Please try again!", {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "light",
+      });
+
+      console.log("Unexpected error occured while signin", error.message);
+    }
+  };
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900/[.40] z-50">
       <div className="bg-white rounded-2xl shadow-xl p-6 w-96 relative">
@@ -20,18 +92,31 @@ const Signin = ({ toggleSignup, onClose }) => {
         <div className="space-y-4">
           <div className="flex flex-col">
             <label className="text-gray-600 text-sm mb-1">Username</label>
-            <Input type="text" placeholder="Enter your username" />
+            <Input
+              type="text"
+              placeholder="Enter your username"
+              ref={usernameRef}
+            />
           </div>
 
           <div className="flex flex-col">
             <label className="text-gray-600 text-sm mb-1">Password</label>
-            <Input type="password" placeholder="Enter your password" />
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              ref={passwordRef}
+            />
           </div>
         </div>
 
+        {error && (
+          <p className="text-center text-sm text-red-600 mt-3">{error}</p>
+        )}
+
         <button
           className="mt-5 w-[20rem] bg-blue-500 text-white py-2 rounded-3xl 
-        hover:bg-blue-600 transition-all duration-300 font-semibold hover:opacity-75 hover:curpo"
+        hover:bg-blue-600 transition-all duration-300 font-semibold hover:opacity-75 hover:cursor-pointer"
+          onClick={handleSignin}
         >
           Sign In
         </button>
@@ -46,6 +131,7 @@ const Signin = ({ toggleSignup, onClose }) => {
           </button>
         </p>
       </div>
+      <ToastContainer />
     </div>
   );
 };
