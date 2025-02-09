5220c77db1d89ebb2bbc38d4ebda8d120779081e
Aditya <adityabasak7031@gmail.com>
2025-02-09 16:35:39 +0530

    add protected route for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
+import { Navigate, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <div className="h-screen bg-blue-50">
-        <Navbar />
-      </div>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Default route to Landing */}
+        <Route path="/" element={<Landing />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
+const ProtectedRoute = () => {
+  const token = localStorage.getItem("token");
+  return token ? <Outlet /> : <Navigate to="/" />;
+};
