0308d5f9471301603a3e3c581670f2d983d2cf56
Aditya <adityabasak7031@gmail.com>
2025-02-09 17:16:31 +0530

    add modal and conditionaly render user and signin button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { GrClose } from "react-icons/gr";
-// import Button from "./Button";
-import Profilebox from "./Profilebox";
+import Button from "./Button";
 import { IoIosArrowDown } from "react-icons/io";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
+import Profilebox from "./Profilebox";
+import axios from "axios";
 
 const Navbar = () => {
   const [openHamburger, setOpenHamburger] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [isSignup, setIsSignup] = useState(true);
+  const [user, setUser] = useState(null);
 
   const menuItems = [
     { name: "Ticket Booking", hasDropdown: true },
@@ -16,6 +22,50 @@ const Navbar = () => {
     { name: "Paytm for Business", hasDropdown: true },
     { name: "Company", hasDropdown: true },
   ];
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/user/me`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = response.data;
+
+        if (!data || !data.user) {
+          console.log(`No data found`);
+          return;
+        }
+
+        setUser(data.user);
+      } catch (error) {
+        console.log(`Error while fetching user: ${error.message}`);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const toggleModal = () => {
+    setShowModal(true);
+    setIsSignup(false);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const toggleSignupSignin = () => {
+    setIsSignup(!isSignup);
+  };
 
   const NavItem = ({ name, hasDropdown }) => (
     <li className="flex justify-between items-center gap-3">
@@ -32,7 +82,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="h-16 w-full p-5 flex md:justify-around justify-between items-center bg-white shadow shadow-gray-200 fixed top-0 z-10">
+      <div className="h-16 w-full p-5 flex md:justify-around justify-between items-center bg-white shadow shadow-gray-200 fixed top-0 z-40">
         <div className="flex items-center justify-center gap-6">
           <button className="text-3xl hover:cursor-pointer md:hidden w-5">
             {openHamburger ? (
@@ -63,15 +113,18 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-
-        {/* <Button lable={"Signup"} /> */}
-        <Profilebox lable={"Aditya"} />
+        {/* User data condition */}
+        {user ? (
+          <Profilebox lable={user.firstName} />
+        ) : (
+          <Button lable="Signin" onclick={toggleModal} />
+        )}
       </div>
 
       {/* Hamburger logic */}
 
       <div
-        className={`fixed top-0 left-0 w-full h-screen bg-white transform transition-transform duration-300 ease-in-out mt-5 ${
+        className={`fixed top-0 left-0 w-full h-screen bg-white transform transition-transform duration-300 ease-in-out mt-5 z-20 ${
           openHamburger ? "translate-x-0" : "-translate-x-full"
         } md:hidden`}
       >
@@ -81,6 +134,25 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
+
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900/[.40] z-50">
+          <div className="bg-white rounded-2xl shadow-xl p-6 w-96 relative">
+            <button
+              className="absolute top-3 right-3 text-black hover:bg-gray-200 p-2 hover:rounded-full transition-all duration-500 ease-in-out hover:cursor-pointer"
+              onClick={closeModal}
+            >
+              <GrClose size={20} />
+            </button>
+
+            {isSignup ? (
+              <Signup toggleSignin={toggleSignupSignin} onClose={closeModal} />
+            ) : (
+              <Signin toggleSignup={toggleSignupSignin} onClose={closeModal} />
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
