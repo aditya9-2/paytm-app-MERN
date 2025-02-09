e511b22caf1fd03d97bd392b2934e9a8c7390e50
Aditya <adityabasak7031@gmail.com>
2025-02-09 16:38:10 +0530

    add /me route

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,12 +4,14 @@ import userSignin from "../controllers/user/userSignin.controller.js";
 import updateUserInformation from "../controllers/user/updateUserInformation.controller.js";
 import userAuthToken from "../middlewares/userAuthMiddleware.js";
 import filerUser from "../controllers/user/filerUser.controller.js";
+import getUserDetails from "../controllers/user/getUserDetails.controller.js";
 
 const router = new Router();
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
 
+router.get('/me', getUserDetails);
 router.put("/update", userAuthToken, updateUserInformation);
 router.get("/bulk", filerUser);
 
