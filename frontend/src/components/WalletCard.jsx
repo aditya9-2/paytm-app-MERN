c2e03959441547eb1eb856cdda402aeda902bb79
Aditya <adityabasak7031@gmail.com>
2025-02-10 19:00:39 +0530

    add user's first name

--- frontend/src/components/WalletCard.jsx
+++ frontend/src/components/WalletCard.jsx
@@ -1,10 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import Button from "./Button";
+import axios from "axios";
 
 const WalletCard = ({ onPayClick, balance }) => {
+  const [username, setUsername] = useState("");
+
+  const userData = async () => {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`http://localhost:3000/api/v1/user/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = response.data;
+
+    setUsername(data.user.firstName);
+  };
+  useEffect(() => {
+    userData();
+  }, []);
   return (
     <div className="w-[30rem] md:w-full h-64 md:mx-8 rounded-2xl bg-slate-50 relative top-[-8rem] shadow-xl border border-gray-500 p-5">
-      <div>
+      <div className="font-bold text-2xl mx-2">
+        Hi, <span className="text-cyan-500">{username}</span>
+      </div>
+      <div className="mt-6">
         <div className="flex gap-3">
           <img
             src="https://pwebassets.paytm.com/frontendcommonweb/static/4cbfc6c4..svg"
@@ -12,14 +34,14 @@ const WalletCard = ({ onPayClick, balance }) => {
           />
           <p className="font-semibold text-sm uppercase">Total Balance</p>
         </div>
-        <span className="mx-14 absolute top-12 font-bold flex gap-1 items-center text-xl">
+        <span className="mx-14 absolute top-12 font-bold flex gap-1 items-center text-xl mt-12">
           <span className="text-cyan-500 font-bold">₹</span>{" "}
           {balance ?? "Loading..."}
         </span>
       </div>
 
-      <div className="absolute right-0 mx-5 top-5">
-        <Button lable={"Pay"} onclick={onPayClick} />
+      <div className="absolute right-0 mx-5 top-19">
+        <Button lable={"Pay"} onClick={onPayClick} />
       </div>
       <div className="w-full bg-gray-300 h-[1px] mt-8 rounded-2xl"></div>
       <p className="text-gray-400 absolute bottom-0 mb-3 left-35 md:left-[39rem] text-sm">
