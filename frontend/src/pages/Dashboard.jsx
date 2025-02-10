d27fe797129f188692acde5d20cf43af2df9ae3d
Aditya <adityabasak7031@gmail.com>
2025-02-10 16:23:29 +0530

    get user balance from BE

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import WalletCard from "../components/WalletCard";
 import PaymentModal from "../components/modal/PaymentModal";
+import axios from "axios";
 
 const Dashboard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/account/balance`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = response.data;
+
+        if (!data) {
+          console.log(`user not found`);
+          return;
+        }
+
+        setBalance(data.balance.toFixed(2));
+      } catch (error) {
+        console.log(`Error while fetching balance: ${error.message}`);
+      }
+    };
+    fetchBalance();
+  }, []);
+
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
@@ -14,7 +46,7 @@ const Dashboard = () => {
       <Navbar />
       <div className="w-full h-[15rem] bg-blue-950 "></div>
       <div className="flex justify-center items-center relative z-0">
-        <WalletCard onPayClick={toggleModal} />
+        <WalletCard onPayClick={toggleModal} balance={balance} />
       </div>
 
       {isModalOpen && <PaymentModal onClose={toggleModal} />}
