36b2a88eaa3f9ae4897d37f9b5c257809979b4d6
Aditya <adityabasak7031@gmail.com>
2025-02-12 19:45:20 +0530

    fix fotter route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
-// import Dashboard from "./pages/Dashboard";
 import { Navigate, Outlet } from "react-router-dom";
 import Update from "./pages/Update";
 import Success from "./pages/Success";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
