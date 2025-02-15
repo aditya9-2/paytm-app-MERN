e48664cf88e4d54e0307efe9aa82afa5e0c4dd0d
Aditya <adityabasak7031@gmail.com>
2025-02-15 20:15:30 +0530

    acces from all 2

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    origin: "https://paytm-app-mern.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
     credentials: true
