4bc12d389e33bb250f8737c2260c774e2569944b
Aditya <adityabasak7031@gmail.com>
2025-02-15 18:52:17 +0530

    update CORS

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+    credentials: true
+}));
 const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*' }))
